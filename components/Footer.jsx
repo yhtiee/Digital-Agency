f5060b486295fcdcd8d9ecf58a62284b175300a2
yhtiee <99039727+yhtiee@users.noreply.github.com>
2023-09-09 05:58:09 +0100

    homepage structure

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,13 +1,70 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const Footer = () => {
+
+  const socials = [
+    "/facebook.png", "/gram.png", "/linkedin.png", "/x.png"
+  ]
+
   return (
-    <section className='w-[100%] h-[200vh] md:h-[100vh] flex flex-col md:flex-row mt-[100px]'>
-        <article className='px-[30px] bg-white w-[100%] md:w-[60%] h-[100vh]'>
-            welcome
+    <section className='w-[100%] flex flex-col md:flex-row mt-[100px]'>
+        <article className='px-[30px] bg-white w-[100%] md:w-[60%] py-[20px]'>
+          <h2 className=' font-poppins font-bold text-[24px] md:text-[32px]'>Contact Us</h2>
+            <form action="" className='border w-[100%] border-[#159EDD] rounded-xl p-4 grid grid-cols-1 gap-[24px] justify-center items-center'>
+              <div className='flex flex-col gap-[8px]'>
+                <label className='font-poppins'>First Name</label>
+                <input type="text" className='border border-[#159EDD] font-poppins rounded-lg p-2 w-[100%]' placeholder='Enter Your First Name'/>
+              </div>
+              <div className='flex flex-col gap-[8px]'>
+                <label className=' font-poppins'>Last Name</label>
+                <input type="text" className='border border-[#159EDD] font-poppins rounded-lg p-2 w-[100%]' placeholder='Enter Your Last Name'/>
+              </div>
+              <div className='flex flex-col gap-[8px]'>
+              <label className=' font-poppins'>Email</label>
+                <input type="email" className='border border-[#159EDD] font-poppins rounded-lg p-2 w-[100%]' placeholder='Enter Your Mail'/>
+              </div>
+              <div className='flex flex-col gap-[8px]'>
+                <label className=' font-poppins'>Message</label>
+                <textarea type="text" className="w-[100%] h-[210px] pl-4 pr-[140px] pt-3 pb-[177px] bg-white rounded-lg border border-[#159EDD] justify-start items-center inline-flex font-poppins text-black" placeholder='Enter Message'/>
+              </div>
+              <div className='w-[100%] flex justify-center items-center'>
+                <button className='rounded-2xl w-[80%] border border-[#159EDD] bg-[#159EDD] px-3 py-2'>
+                  <p className='text-white font-poppins'>Submit</p>
+                </button>
+              </div>
+            </form>
         </article>
-        <article className='px-[30px] bg-[#000000] w-[100%] md:w-[40%] h-[100vh]'>
-            welcome
+        <article className='px-[30px] bg-[#000000] w-[100%] md:w-[40%] flex flex-col justify-center items-center gap-[64px] py-[30px] md:pyy-[0px]'>
+            <div className='flex flex-col gap-[1.5rem]'>
+              <Link className=' text-white font-poppins font-semibold text-center' href="#">
+                Support & FAQs
+              </Link>
+              <Link className=' text-white font-poppins font-semibold text-center' href="#">
+                Email
+              </Link>
+              <Link className=' text-white font-poppins font-semibold text-center' href="#">
+                Phone Number 1
+              </Link>
+              <Link className=' text-white font-poppins font-semibold text-center' href="#">
+                Phone Number 1
+              </Link>
+              <Link className=' text-white font-poppins font-semibold text-center' href="#">
+                Nigeria
+              </Link>
+            </div>
+            <div className='flex flex-row gap-[1rem]'>
+              {socials.map((items, index) => (
+                <Link key={index} href="#">
+                  <Image src={items} width={500} height={100} className='w-[30px] h-[30px] md:w-[40px] md:h-[40px]'/>
+                </Link>
+              ))}
+            </div>
+            <div className='flex flex-col gap-[1rem]'>
+              <p className=' font-poppins font-medium text-white text-center'>© 2012 - 2023 DigitalWakaa. All Rights Reserved.</p>
+              <p className=' font-poppins font-medium text-white text-center'>Our Privacy and Cookie Policies and Terms</p>
+            </div>
         </article>
     </section>
   )
--- components/HomepageHeader.jsx
+++ components/HomepageHeader.jsx
@@ -2,8 +2,21 @@ import React from 'react'
 
 const HomepageHeader = () => {
   return (
-    <section className='homepage-header h-[100vh] w-[100%]'>
-        welcome
+    <section className='homepage-header h-[100vh] w-[100%] flex justify-center items-center'>
+        <article className='flex flex-col gap-[2rem]'>
+          <div>
+            <h1 className='header-text text-center font-poppins font-bold text-[42px] md:text-[64px]'>We're Thrilled You Found Us!</h1>
+            <p className='text-center font-poppins font-bold text-[24px] text-white'>Now that you are here, let's turn your online goals into achievements.</p>
+          </div>
+          <div className='w-[100%] flex justify-center items-center gap-[1.5rem]'>
+            <button className='rounded-2xl bg-[#2faf3a] px-3 py-2'>
+              <p className='text-white font-poppins font-medium text-[18px]'>Get Started</p>
+            </button>
+            <button className='rounded-2xl border bg-transparent px-3 py-2'>
+              <p className='text-white font-poppins font-medium text-[18px]'>Learn More</p>
+            </button>
+          </div>
+        </article>
     </section>
   )
 }
